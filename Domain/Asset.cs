eb61a42906ac4cfcad427bfff2f23b8352f03de8
Mahmoud <mahmoud@getopenwater.com>
2022-12-16 21:12:13 +0200

    add data & domain class library & asset class

---
+++ Domain/Asset.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Asset
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? AssetJson { get; set; }
+    }
+}
