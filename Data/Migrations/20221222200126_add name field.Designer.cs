e0a54653b05a8251d9ccdfec4c7b3ddab370da59
Mahmoud <mahmoud@getopenwater.com>
2022-12-23 14:02:49 +0200

    add migration for the name property

--- Data/Migrations/20221222200126_add name field.Designer.cs
+++ Data/Migrations/20221222200126_add name field.Designer.cs
@@ -3,6 +3,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(MenaPharmCompanyContext))]
-    partial class MenaPharmCompanyContextModelSnapshot : ModelSnapshot
+    [Migration("20221222200126_add name field")]
+    partial class addnamefield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
