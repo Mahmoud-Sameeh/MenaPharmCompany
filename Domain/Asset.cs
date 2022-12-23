b80f7c031677ce339e73290c6e2b5176b58b732a
Mahmoud <mahmoud@getopenwater.com>
2022-12-23 15:54:30 +0200

    make some props required

--- Domain/Asset.cs
+++ Domain/Asset.cs
@@ -6,7 +6,9 @@ namespace Domain
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter asset Name")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Please enter asset properties")]
         public string? AssetJson { get; set; }
     }
 }
